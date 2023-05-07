a7966e1cd4b77d85f1add18003ba9df1ffa00ea6
Gurinder31 <109625259+Gurinder31@users.noreply.github.com>
2023-05-07 14:50:04 +0530

    Update app.module.ts

--- FrontendEcommerce/src/app/app.module.ts
+++ FrontendEcommerce/src/app/app.module.ts
@@ -7,13 +7,18 @@ import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginGuardGuard } from './guards/login-guard.guard';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    HeaderComponent,
+    FooterComponent,.
   ],
   providers: [LoginGuardGuard],
   bootstrap: [AppComponent],
