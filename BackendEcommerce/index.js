c5f318b9fb12792571406e3e0f5c4877c7e4a742
amanroy007 <roya5846@gmail.com>
2023-06-20 19:54:36 +0530

    runn server

--- BackendEcommerce/index.js
+++ BackendEcommerce/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import {
-  buildConnection,
-  registerUserData,
-  findExistingUser,
-} from "./connectionDB.js";
+// import {
+//   buildConnection,
+//   registerUserData,
+//   findExistingUser,
+// } from "./connectionDB.js";
 
 import productsList from './allProducts.js';
 
@@ -28,65 +28,65 @@ app.get('/products',function(req,res){
 })
 
 //Post request for login user;
-app.post("/login", function (request, response) {
-  console.log(request.body.email);
-  let email = request.body.email;
-  let password = request.body.password;
-  if (!email && !password) return;
-  buildConnection().then((resolve) => {
-    let query = { email: email, securityKey: password };
-    findExistingUser(query).then((resolve) => {
-      if (resolve.length > 0) {
-        response.status(200).json({ email: email, isLogedIn: true });
-      } else {
-        response
-          .status(201)
-          .json({
-            email: email,
-            isLogedIn: false,
-            errorMsg: "Check your email and password",
-          });
-      }
-    });
-  });
-});
+// app.post("/login", function (request, response) {
+//   console.log(request.body.email);
+//   let email = request.body.email;
+//   let password = request.body.password;
+//   if (!email && !password) return;
+//   buildConnection().then((resolve) => {
+//     let query = { email: email, securityKey: password };
+//     findExistingUser(query).then((resolve) => {
+//       if (resolve.length > 0) {
+//         response.status(200).json({ email: email, isLogedIn: true });
+//       } else {
+//         response
+//           .status(201)
+//           .json({
+//             email: email,
+//             isLogedIn: false,
+//             errorMsg: "Check your email and password",
+//           });
+//       }
+//     });
+//   });
+// });
 
 // POST request for register user
-app.post("/register", function (request, response) {
-  let { firstName, lastName, email, password } = request.body;
-  if (!firstName && !lastName && !email && !password)
-    response.status(404).json({ message: "Data not found" });
-  buildConnection().then((resolve) => {
-    let payload = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      securityKey: password,
-    };
-    let query = { email: email };
-    findExistingUser(query).then((resolve) => {
-      if (resolve.length > 0) {
-        response
-          .status(201)
-          .json({ message: "User already registered", isExistingUser: true });
-      } else {
-        registerUserData(payload)
-          .then((resolve) => {
-            response.status(200).json({
-              message: "User Registered Successfully",
-              isRegisterd: true,
-            });
-          })
-          .catch((error) => {
-            response.status(501).json({
-              message: "Not able to Register User",
-              error: error,
-              isRegisterd: false,
-            });
-          });
-      }
-    });
-  });
-});
+// app.post("/register", function (request, response) {
+//   let { firstName, lastName, email, password } = request.body;
+//   if (!firstName && !lastName && !email && !password)
+//     response.status(404).json({ message: "Data not found" });
+//   buildConnection().then((resolve) => {
+//     let payload = {
+//       firstName: firstName,
+//       lastName: lastName,
+//       email: email,
+//       securityKey: password,
+//     };
+//     let query = { email: email };
+//     findExistingUser(query).then((resolve) => {
+//       if (resolve.length > 0) {
+//         response
+//           .status(201)
+//           .json({ message: "User already registered", isExistingUser: true });
+//       } else {
+//         registerUserData(payload)
+//           .then((resolve) => {
+//             response.status(200).json({
+//               message: "User Registered Successfully",
+//               isRegisterd: true,
+//             });
+//           })
+//           .catch((error) => {
+//             response.status(501).json({
+//               message: "Not able to Register User",
+//               error: error,
+//               isRegisterd: false,
+//             });
+//           });
+//       }
+//     });
+//   });
+// });
 
 app.listen(PORT, () => console.log("Server is running /👍"));
